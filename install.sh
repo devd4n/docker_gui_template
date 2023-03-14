02579544aef883c83d3ee905c6bd419e83baa9ac
devd4n <18461526+devd4n@users.noreply.github.com>
2023-03-14 15:03:07 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl https://raw.githubusercontent.com/devd4n/docker_starter/master/Makefile -o Makefile
-curl https://raw.githubusercontent.com/devd4n/docker_starter/master/xephyrdocker -o xephyrdocker
+curl https://raw.githubusercontent.com/devd4n/docker_starter/main/Makefile -o Makefile
+curl https://raw.githubusercontent.com/devd4n/docker_starter/main/xephyrdocker -o xephyrdocker
 chmod +x xephyrdocker
 
