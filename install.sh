08945e2aefa1f29b801b4f32ea7152e66a636920
devd4n <18461526+devd4n@users.noreply.github.com>
2023-03-14 14:54:57 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-apt install make
 curl https://github.com/devd4n/docker_starter/Makefile -o Makefile
 curl https://github.com/devd4n/docker_starter/Makefile -o Makefile
 chmod +x xephyrdocker
